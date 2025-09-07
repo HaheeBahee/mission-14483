db3f665482aaa4634faa87f6c4819e1a6ad36730
HaheeBahee <parkadg@naver.com>
2025-09-07 23:37:32 +0900

    주석 추가

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -5,16 +5,17 @@ public class Calc {
 
     public static int run(String expression) {
 
+        //공백 기준으로 분리
         String[] parts = expression.split(" ");
-        System.out.println(parts);
 
+        //항이 3개 이상인 경우
         if (parts.length > 3) {
             int partial = 0;
             if (parts[1].equals("+")) {
                 partial = add(parts);
             } else if (parts[1].equals("-")) {
                 partial = minus(parts);
-            } else if (parts[1].equals("*")){
+            } else if (parts[1].equals("*")) {
                 partial = multiply(parts);
             }
             StringBuilder nextExpression = new StringBuilder();
@@ -22,9 +23,10 @@ public class Calc {
             for (int i = 3; i < parts.length; i++) {
                 nextExpression.append(" ").append(parts[i]);
             }
-            return run(nextExpression.toString());
+            return run(nextExpression.toString()); //새로 만든 식을 다시 run()에 넘겨 재귀 호출
         }
 
+        //base case : 항이 정확히 3개인 경우
         int left = Integer.parseInt(parts[0]);
         int right = Integer.parseInt(parts[2]);
 
@@ -35,6 +37,8 @@ public class Calc {
         return 0;
     }
 
+
+    //더하기
     private static int add(String[] parts) {
         int left = Integer.parseInt(parts[0]);
         int right = Integer.parseInt(parts[2]);
@@ -42,6 +46,7 @@ public class Calc {
         return left + right;
     }
 
+    //빼기
     private static int minus(String[] parts) {
         int left = Integer.parseInt(parts[0]);
         int right = Integer.parseInt(parts[2]);
@@ -49,6 +54,7 @@ public class Calc {
         return left - right;
     }
 
+    //곱하기
     private static int multiply(String[] parts) {
         int left = Integer.parseInt(parts[0]);
         int right = Integer.parseInt(parts[2]);
